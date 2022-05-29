9d60dae56d844bf406f23684729e00766be14a65
tubi55 <tubi55@nate.com>
2022-05-29 14:24:54 +0900

    프론트에서 백단으로 요청을 보내고 서버에서 프론트로 응답

--- node/index.js
+++ node/index.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const mongoose = require('mongoose');
 const port = 5000;
 
 app.use(express.static(path.join(__dirname, '../react/build')));
 
+//클라이언트에서 보내는 데이터를 받도록 설정 (body-parser)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(port, () => {
-	console.log('Server listening on port:' + port);
+	mongoose
+		.connect('mongodb+srv://aaa:!abcd1234@cluster0.shsbo8p.mongodb.net/?retryWrites=true&w=majority')
+		.then(() => {
+			console.log('Server listening on port:' + port);
+			console.log('mongoDB success');
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 });
 
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '../react/build/index.html'));
+});
+
+//react로부터 받은 요청처리
+app.post('/api/send', (req, res) => {
+	console.log(req.body);
+	res.status(200).json({ success: true, result: req.body.name + '2' });
 });
--- react/src/App.js
+++ react/src/App.js
@@ -1,4 +1,21 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+
 function App() {
+	const item = {
+		name: 'Emily',
+	};
+	useEffect(() => {
+		axios
+			.post('/api/send', item)
+			.then((res) => {
+				console.log(res);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
+
 	return <h1>React Project</h1>;
 }
 
