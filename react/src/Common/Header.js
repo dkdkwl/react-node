c1481a15b9b250f16998070a7487dfd6613adb11
tubi55 <tubi55@nate.com>
2022-06-11 12:59:57 +0900

    header 스타일링

--- react/src/Common/Header.js
+++ react/src/Common/Header.js
@@ -1,21 +1,53 @@
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const HeaderWrapper = styled.header`
+	width: 350px;
+	height: 100vh;
+	position: fixed;
+	background: #222;
+	top: 0px;
+	left: 0%;
+	padding: 50px;
+
+	h1 {
+		margin-bottom: 40px;
+		a {
+			font: 50px/1 'arial';
+			color: #fff;
+		}
+	}
+`;
+
+const Gnb = styled.ul`
+	a {
+		display: block;
+		padding: 10px;
+		font: bold 16px/1 'arial';
+		color: #bbb;
+
+		&:hover {
+			color: hotpink;
+		}
+	}
+`;
 
 function Header() {
 	return (
-		<header>
+		<HeaderWrapper>
 			<h1>
 				<NavLink to='/'>LOGO</NavLink>
 			</h1>
 
-			<ul id='gnb'>
+			<Gnb>
 				<li>
 					<NavLink to='/list'>Show List</NavLink>
 				</li>
 				<li>
 					<NavLink to='/create'>Write Post</NavLink>
 				</li>
-			</ul>
-		</header>
+			</Gnb>
+		</HeaderWrapper>
 	);
 }
 
